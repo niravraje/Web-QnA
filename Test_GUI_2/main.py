b9948d690d2946d6d557cee98e12b3db061ee243
niravraje <rajenirav@gmail.com>
2020-08-30 23:06:13 +0530

    Added infersent_glove_context_generation.py module. Addition of Colab files pending.

--- Test_GUI_2/main.py
+++ Test_GUI_2/main.py
@@ -1,15 +1,27 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+import bag_of_words
 
 app = Flask(__name__)
 
-@app.route("/")
+
+@app.route('/')
 def home():
-    render_template("home.html")
+    return render_template('home.html')
 
-@app.route("/page2")
-def page2():
-    return "Hi, this is the 2nd page."
 
-if __name__ == "__main__":
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+
+@app.route('/handle_data', methods=['GET', 'POST'])
+def handle_data():
+    url = request.form['inputURL']
+    question = request.form['inputQuestion']
+    answer = bag_of_words.get_answer(url, question)
+
+    return render_template('handle_data.html', result_url=url, result_question=question,
+                           result_answer=answer)
+
+if __name__ == '__main__':
     app.run(debug=True)
-
